f3c935f58b3de60287773bd29e14ca620260fdd5
yajunxuejue <352346950@qq.com>
2017-10-16 11:32:38 +0800

    Update sim_DSM_RV.m

--- sim_DSM_RV.m
+++ sim_DSM_RV.m
@@ -110,18 +110,3 @@ function [sim_result, command_speed] = simPointMDS( obs_points, init_pose, targe
       %end
   end
 end
-function plot = plotResult(x, y)
-  hold on;
-  plot(x, y,'ok','markersize',2,'linewidth',7.5);
-end
-function xd = globallyStableDS(init_p, target_p)
- K = 5;
- if (target_p - init_p)<0.01
-     xd = 0;
- else
-     xd = 10*(target_p - init_p);%/norm(target_p - init_p);%
- end   
-end
-function [] = plotCircle(center_x, center_y, radius)
-
-end
