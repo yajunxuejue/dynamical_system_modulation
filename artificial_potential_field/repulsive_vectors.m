18a5f9322134af1d510e95e653b7273d790778ee
yajunxuejue <352346950@qq.com>
2017-10-16 12:58:54 +0800

    Update repulsive_vectors.m

--- artificial_potential_field/repulsive_vectors.m
+++ artificial_potential_field/repulsive_vectors.m
@@ -1,1 +1,6 @@
-
+function rep_vec = calculateModulationSpeed(control_point, obstacle_points)
+%DISTANCE_CALCULATE Summary of this function goes here
+%   Detailed explanation goes here
+obs_pt_con_pt = control_point - obstacle_points(:,i);
+rep_vec = (2./(1+exp((6.7*norm(obs_pt_con_pt)-1)*8)))*obs_pt_con_pt/distance;
+end
